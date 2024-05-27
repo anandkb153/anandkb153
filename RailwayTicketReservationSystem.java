8861d160185c3755bcd3ba55118656f3c951d31e
anandkb153 <111950372+anandkb153@users.noreply.github.com>
2024-05-27 20:31:01 +0530

    Update RailwayTicketReservationSystem.java

--- RailwayTicketReservationSystem.java
+++ RailwayTicketReservationSystem.java
@@ -3,6 +3,19 @@ package ZOHO_L3_Programs;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Passenger {
+    String name;
+    int age;
+    String gender;
+    String berthPreference;
+
+    public Passenger(String name, int age, String gender, String berthPreference) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.berthPreference = berthPreference;
+    }
+}
 public class RailwayTicketReservationSystem {
 	private int totalBerths=3;
 	private int lowerBerths = 1;
